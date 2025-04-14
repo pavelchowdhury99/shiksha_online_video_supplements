e37652afd9983d1dc1a00c1becd09b7fcb2b7814
pavelchowdhury99 <59216690+pavelchowdhury99@users.noreply.github.com>
2025-04-14 23:18:01 +0530

    googlesheet_custom_function_python

---
+++ googlesheet_custom_function_python/main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+from pydantic import BaseModel
+from shapely import wkb
+
+
+class WKB_HEX(BaseModel):
+    wkb_hex: str
+
+
+def convert_wkb_wkt(text: str) -> str:
+    out = wkb.loads(text, hex=True)
+    return str(out)
+
+app = FastAPI()
+
+@app.post("/get-wkt-from-wkb")
+def get_wkt_from_wkb(inp:WKB_HEX):
+    return convert_wkb_wkt(inp.wkb_hex)
+
+# redirecting home page to /docs
+@app.get("/", include_in_schema=False)
+async def docs_redirect():
+    return RedirectResponse(url='/docs')
+
+if __name__ == '__main__':
+    print(convert_wkb_wkt('010100000000b884a9329436409c878310301e5640'))
